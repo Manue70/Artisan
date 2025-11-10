0ed10c3d60e00d1f0ee6b44561a4555c019c05aa
ManueP <pineramanuela@ymail.com>
2025-11-10 11:35:35 +0100

    Ajout du .env, db.js et mise à jour backend

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,15 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'art_exo',
-  password: 'Artisan123',
-  database: 'artisans_db',
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 export default db;
+
 
